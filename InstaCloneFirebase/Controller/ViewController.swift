ee87d9b41c03945f1d71800866b415c2f06470b0
busrademirbasozkan <busrademirbasozkan@gmail.com>
2022-11-20 19:27:45 +0300

    Userinterface oluşturuldu.

--- InstaCloneFirebase/Controller/ViewController.swift
+++ InstaCloneFirebase/Controller/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    
+    @IBAction func signinButton(_ sender: Any) {
+        performSegue(withIdentifier: "toTabBar", sender: nil)
+    }
+    
+    
+    @IBAction func signupButton(_ sender: Any) {
+    }
+    
 }
 
