35897de2a7dafde2530a6c8034eb62e15accda11
busrademirbasozkan <busrademirbasozkan@gmail.com>
2022-11-23 15:40:00 +0300

    .gitignore eklendi.

--- InstaCloneFirebase/Controller/UploadViewController.swift
+++ InstaCloneFirebase/Controller/UploadViewController.swift
@@ -48,6 +48,26 @@ class UploadViewController: UIViewController, UIImagePickerControllerDelegate, U
     
     @IBAction func uploadButton(_ sender: Any) {
         
+        let storage = Storage.storage()
+        let storageReference = storage.reference()
+        
+        let mediaFolder = storageReference.child("media")
+        
+        if let data = imageView.image?.jpegData(compressionQuality: 0.5){
+            let imageReference = mediaFolder.child("image.jpg")
+            imageReference.putData(data,metadata: nil) { (metadata, error) in
+                if error != nil {
+                    print(error?.localizedDescription)
+                }else{
+                    imageReference.downloadURL { (url, error) in
+                        if error != nil {
+                            let imageUrl = url?.absoluteString
+                            print(imageUrl)
+                        }
+                    }
+                }
+            }
+        }
         
         
     }
